2910f471c04361e4dcdc6dca73fb844fff542ad8
Ivan Vieira <ivanjr.ce@gmail.com>
2026-03-14 17:40:02 -0300

    Add Base Entity e ApplicationDbContext e pastas auxiliares

---
+++ AppProject.Core.Infrastructure.DB/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppProject.Core.Infrastructure.DB;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    // DbSets for your Entities
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
